20a9cc2fdc007e7eb6cd4bfd37aa0f305386ed44
lkoblue <gangwon20@gmail.com>
2024-11-17 21:27:01 +0900

    최초커밋

---
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
@@ -0,0 +1,13 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+public class ConcreteClient {
+    private ConcreteLogic logic;
+
+    public ConcreteClient(ConcreteLogic logic) {
+        this.logic = logic;
+    }
+
+    public void execute() {
+        logic.operation();
+    }
+}
